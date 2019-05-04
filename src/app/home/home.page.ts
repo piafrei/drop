dafe59fd4f37aee67a7c71826931f0a964902868
Pia Freilinger <pia.freilinger@gmx.de>
2019-05-04 23:14:00 +0200

    Working version of google maps integration

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+
+declare var google: any;
 
 @Component({
   selector: 'app-home',
@@ -6,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage implements OnInit {
-
+  @ViewChild('map') mapElement: ElementRef;
+  map: any;
   constructor() { }
-
   ngOnInit() {
+      this.loadMap();
   }
+
+  loadMap() {
+
+    const latLng = new google.maps.LatLng(-34.9290, 138.6010);
+
+    const mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+
+    }
 }
