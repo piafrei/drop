8143c1076875dbe839f8d7048e3f5ab136cfa014
wielanno <noe.wieland.16@lehre.mosbach.dhbw.de>
2019-05-04 13:20:48 +0200

    added googlemaps module and component, but not working

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,23 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { IonicModule } from "@ionic/angular";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
 
-import { HomePage } from './home.page';
+import { HomePage } from "./home.page";
+import { SharedModule } from "../shared/shared.module";
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     FormsModule,
     IonicModule,
     RouterModule.forChild([
       {
-        path: '',
+        path: "",
         component: HomePage
       }
     ])
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,14 +1,80 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Capacitor, Plugins, GeolocationPosition } from "@capacitor/core";
+import { Observable, of, from as fromPromise } from "rxjs";
+import { tap, map, switchMap } from "rxjs/operators";
+
+import { LoadingController, AlertController } from "@ionic/angular";
+
+const { Toast, Geolocation } = Capacitor.Plugins;
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage implements OnInit {
+  public coordinates$: Observable<GeolocationPosition>;
+  public defaultPos: { latitude: 45; longitude: 9 };
 
-  constructor() { }
+  constructor(
+    public loading: LoadingController,
+    public alertCtrl: AlertController
+  ) {}
 
   ngOnInit() {
+    // start the loader
+    this.displayLoader().then((loader: any) => {
+      //get position
+      return (
+        this.getCurrentPosition()
+          .then(position => {
+            //close the loader + return position
+            loader.dismiss();
+            return position;
+          })
+          //if error
+          .catch(err => {
+            //close loader + return Null
+            loader.dismiss();
+            return null;
+          })
+      );
+    });
+  }
+
+  async displayLoader() {
+    const loading = await this.loading.create({});
+    await loading.present();
+    return loading;
+  }
+
+  private async presentAlert(message: string): Promise<HTMLIonAlertElement> {
+    const alert = await this.alertCtrl.create({
+      header: "Alert",
+      subHeader: "We are offline",
+      message: message,
+      buttons: ["OK"]
+    });
+    await alert.present();
+    return alert;
+  }
+
+  private async getCurrentPosition(): Promise<any> {
+    const isAvailable: boolean = Capacitor.isPluginAvailable("Geolocation");
+    if (!isAvailable) {
+      console.log("Err: plugin is not available");
+      return of(new Error("Err: plugin not available"));
+    }
+    const POSITION = Plugins.Geolocation.getCurrentPosition()
+      //handle Capacitors Errors
+      .catch(err => {
+        console.log("ERR", err);
+        return new Error(err.message || "customized message");
+      });
+    this.coordinates$ = fromPromise(POSITION).pipe(
+      switchMap((data: any) => of(data.coords)),
+      tap(data => console.log(data))
+    );
+    return POSITION;
   }
 }
