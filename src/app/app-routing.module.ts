f1af9055aa68a957a383f44d121dac45f0902c1d
mhofherr <hofherr@quellwerke.de>
2019-05-06 16:17:28 +0200

    frontend styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
   { path: 'add-drop', loadChildren: './pages/add-drop/add-drop.module#AddDropPageModule' },
-  { path: 'new-drop/:id', loadChildren: './pages/add-drop/add-drop.module#AddDropPageModule' },
+  { path: 'details/:id', loadChildren: './pages/add-drop/add-drop.module#AddDropPageModule' },
   { path: 'new-drop', loadChildren: './pages/add-drop/add-drop.module#AddDropPageModule' },
   { path: 'filter', loadChildren: './pages/filter/filter.module#FilterPageModule' },
   { path: 'notifications', loadChildren: './pages/notifications/notifications.module#NotificationsPageModule' },
