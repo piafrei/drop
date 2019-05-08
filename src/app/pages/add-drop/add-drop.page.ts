d9efcecd9b28c785dd0ae7db496efce3c8c5ce2c
Pia Freilinger <pia.freilinger@gmx.de>
2019-05-08 15:15:24 +0200

    DROP-12 hide drop when score is under -10

--- src/app/pages/add-drop/add-drop.page.ts
+++ src/app/pages/add-drop/add-drop.page.ts
@@ -55,10 +55,14 @@ export class AddDropPage implements OnInit {
     await loading.present();
 
     if (this.dropId) {
+      if (this.drop.score <= -10) {
+      this.dropService.removeDrop(this.dropId);
+      } else {
       this.dropService.updateDrop(this.drop, this.dropId).then(() => {
         loading.dismiss();
         this.nav.back();
       });
+      }
     } else {
       this.dropService.addDrop(this.drop).then(() => {
         loading.dismiss();
