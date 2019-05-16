96b25df3d6f7df9ddd77df612599e87df430bd6e
wielanno <noe.wieland.16@lehre.mosbach.dhbw.de>
2019-05-16 10:03:48 +0200

    Remove AGBs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,18 +2,39 @@ import { NgModule } from "@angular/core";
 import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: './pages/home/home.module#HomePageModule' },
-  { path: 'home/add-drop', loadChildren: './pages/add-drop/add-drop.module#AddDropPageModule' },
+  { path: "", redirectTo: "home", pathMatch: "full" },
+  { path: "home", loadChildren: "./pages/home/home.module#HomePageModule" },
+  {
+    path: "home/add-drop",
+    loadChildren: "./pages/add-drop/add-drop.module#AddDropPageModule"
+  },
   // { path: 'drop-detail/:id', loadChildren: './pages/drop-detail/drop-detail.module#DropDetailPage' },
-  { path: 'detail/:id', loadChildren: './pages/add-drop/add-drop.module#AddDropPageModule' },
-  { path: 'home/new-drop', loadChildren: './pages/add-drop/add-drop.module#AddDropPageModule' },
-  { path: 'home/filter', loadChildren: './pages/filter/filter.module#FilterPageModule' },
-  { path: 'home/notifications', loadChildren: './pages/notifications/notifications.module#NotificationsPageModule' },
-  { path: 'home/settings', loadChildren: './pages/settings/settings.module#SettingsPageModule' },
-  { path: 'home/account', loadChildren: './pages/account/account.module#AccountPageModule' },
-  { path: 'drop/:id', loadChildren: './pages/drop/drop.module#DropPageModule' },
-
+  {
+    path: "detail/:id",
+    loadChildren: "./pages/add-drop/add-drop.module#AddDropPageModule"
+  },
+  {
+    path: "home/new-drop",
+    loadChildren: "./pages/add-drop/add-drop.module#AddDropPageModule"
+  },
+  {
+    path: "home/filter",
+    loadChildren: "./pages/filter/filter.module#FilterPageModule"
+  },
+  {
+    path: "home/notifications",
+    loadChildren:
+      "./pages/notifications/notifications.module#NotificationsPageModule"
+  },
+  {
+    path: "home/settings",
+    loadChildren: "./pages/settings/settings.module#SettingsPageModule"
+  },
+  {
+    path: "home/account",
+    loadChildren: "./pages/account/account.module#AccountPageModule"
+  },
+  { path: "drop/:id", loadChildren: "./pages/drop/drop.module#DropPageModule" },
 
   {
     path: "home/settings/about",
@@ -27,11 +48,6 @@ const routes: Routes = [
   {
     path: "home/settings/privacy",
     loadChildren: "./pages/settings/privacy/privacy.module#PrivacyPageModule"
-  },
-  {
-    path: "home/settings/termsofuse",
-    loadChildren:
-      "./pages/settings/termsofuse/termsofuse.module#TermsofusePageModule"
   }
 ];
 
