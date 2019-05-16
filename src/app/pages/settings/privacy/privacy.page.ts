e24a34149fc39aae3ff10796bda471f53f3bf47d
wielanno <noe.wieland.16@lehre.mosbach.dhbw.de>
2019-05-16 11:33:22 +0200

    Add Read more Button on privacy page

--- src/app/pages/settings/privacy/privacy.page.ts
+++ src/app/pages/settings/privacy/privacy.page.ts
@@ -8,5 +8,16 @@ import { Component, OnInit } from "@angular/core";
 export class PrivacyPage implements OnInit {
   constructor() {}
 
+  public show: boolean = false;
+  public buttonName: any = "Show";
+
+  toggle() {
+    this.show = !this.show;
+
+    // CHANGE THE NAME OF THE BUTTON.
+    if (this.show) this.buttonName = "Hide";
+    else this.buttonName = "Show";
+  }
+
   ngOnInit() {}
 }
