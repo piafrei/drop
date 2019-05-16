50f62cdef97662950b33539958c522e7f063ef48
Pia Freilinger <pia.freilinger@gmx.de>
2019-05-16 16:53:45 +0200

    DROP-95 add User only if not already in DB

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import {AngularFirestore, AngularFirestoreCollection} from '@angular/fire/firestore';
 import {Observable} from 'rxjs';
-import {map} from 'rxjs/operators';
+import {combineLatest, map} from 'rxjs/operators';
 import {Device} from '@ionic-native/device/ngx';
-import {forEach} from '@angular-devkit/schematics';
 
 export interface User {
     id?: string;
@@ -36,28 +35,35 @@ export class UserService {
     return this.user;
   }
 
-  getUser(id) {
-    return this.userCollection.doc<User>(id).valueChanges();
-  }
-
-  updateUser(user: User, id: string) {
-    return this.userCollection.doc(id).update(user);
-  }
-
   addUser() {
     const deviceId = this.device.uuid;
-    const users = this.getAllUsers();
+    const users = this.userCollection;
+    let userArray = null;
 
-    users.forEach(function (value) {
-    });
-    const user: User = {
-     deviceID: deviceId,
-     visibleDrops: []
-   };
-    this.userCollection.add(user);
+    let userExists = false;
+    if (users != null) {
+    this.user.subscribe((products) => {
+       userArray = products;
+     });
+
+    if (userArray != null) {
+    for (const userElement of userArray) {
+      if (userElement.deviceID === deviceId) {
+         userExists = true;
+      }
+    }
+    if (userExists === false) {
+      const user: User = {
+       deviceID: deviceId,
+       visibleDrops: []
+      };
+      this.userCollection.add(user);
+    }
+    }
+    }
   }
 
-  removeUser(id) {
-     return this.userCollection.doc(id).delete();
-  }
+    saveDrop() {
+
+    }
 }
