2c35cba2e452700bc1c1e5a82491ea64951f491d
Pia Freilinger <pia.freilinger@gmx.de>
2019-05-22 12:00:55 +0200

    add Filter page and save the choice of the user

--- src/app/pages/filter/filter.page.ts
+++ src/app/pages/filter/filter.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Drop} from '../../services/drop.service';
+import {FormArray, FormBuilder, FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'app-filter',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilterPage implements OnInit {
 
-  constructor() { }
+  filters = [{ name: 'EssenTrinken' }, { name: 'KulturSehenswuerdigkeiten' }, { name: 'Einkaufen' }, { name: 'NicePlace' }, { name: 'HiddenGem' }, { name: 'Ueberraschung' }];
+  filterForm: FormGroup;
+  filterFormArray;
 
-  ngOnInit() {
+  constructor(private fb: FormBuilder) {
   }
 
+  ngOnInit() {
+    this.filterForm = this.fb.group({
+      activeFilter: this.fb.array([])
+    });
+  }
+
+  saveFilterToFilterFormArray(filter: string, isChecked: boolean) {
+    this.filterFormArray = <FormArray>this.filterForm.controls.activeFilter;
+    if (isChecked) {
+      this.filterFormArray.push(new FormControl(filter));
+    } else {
+      const index = this.filterFormArray.controls.findIndex(x => x.value === filter);
+      this.filterFormArray.removeAt(index);
+    }
+  }
+
+  submitSelectedFilter() {
+  console.log('Active Filter to submit ' + this.filterFormArray.getRawValue());
+  }
 }
