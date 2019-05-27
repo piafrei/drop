8f9713482b9bb21b6415282b3349c103ce721a56
Selina Gross <selina.gross@web.de>
2019-05-27 16:52:31 +0200

    Merge branch 'master' of https://github.com/piafrei/drop
    # Conflicts:
    #	src/app/pages/settings/settings.page.html
    #	src/theme/base.scss

--- src/app/pages/settings/privacy/privacy.page.ts
+++ src/app/pages/settings/privacy/privacy.page.ts
@@ -9,14 +9,14 @@ export class PrivacyPage implements OnInit {
   constructor() {}
 
   public show: boolean = false;
-  public buttonName: any = "Show";
+  public buttonName: any = "Mehr lesen";
 
   toggle() {
     this.show = !this.show;
 
     // CHANGE THE NAME OF THE BUTTON.
-    if (this.show) this.buttonName = "Hide";
-    else this.buttonName = "Show";
+    if (this.show) this.buttonName = "Weniger lesen";
+    else this.buttonName = "Mehr lesen";
   }
 
   ngOnInit() {}
